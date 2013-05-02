b6895d0b091640957a3981900c49adb7f7bd74d4
Dan Miller <daniel.miller@jhmi.edu>
2013-05-02 14:50:10 -0400

    Version 0.1 alpha code.

--- js/jquery.modalHome.js
+++ js/jquery.modalHome.js
@@ -1,48 +1,120 @@
 /*global $:true,jQuery:true*/
 /*jslint debug: true, devel: true, evil: true, vars: true, sloppy: true, undef: true */
 (function ($) {
-/*
+
 	var methods = {
 		init : function (options) {
-		
 			// public / defaults
 			var settings = $.extend({
+                'content' : '',
 				'tableClass' : 'responsive',
 				'minWidth' : 767
 			}, options);
-			
+
+            var modalBG = $('.jhm-modal-bg'),
+                modal = $('.jhm-modal');
+
+            if (modalBG.length === 0) {
+                modalBG = createBg();
+            }
+            if (modal.length === 0) {
+                modal = createModal();
+            }
+            // load static html
+            if (settings.content.length > 0) {
+                populateModal(settings.content);
+            }
+            
+            $('.jhm-modal-close').on('click', function () {
+                console.log('close click');
+                modal.trigger('jhm.modal.close');
+            });
+            
+            console.log('setup events');
+            // close event
+            modal.on('jhm.modal.close', function () {
+                console.log('jhm.modal.close');
+                hideModal(modal);
+                modal.off('jhm.modal.close');
+            });
+            // open event
+            modal.on('jhm.modal.open', function () {
+                console.log('jhm.modal.open');
+                revealModal(modal);
+                modal.off('jhm.modal.open');
+            });
+            // esc press event
+            $('body').on('keyup', function (e) {
+                if (e.which === 27) {
+                    modal.trigger('jhm.modal.close');
+                }
+			});
+            // bg click close
+            $('.jhm-modal-bg').on('click', function () {
+                modal.trigger('jhm.modal.close');
+            });
+            
+            // show immediately
+            modal.trigger('jhm.modal.open');
+            /*
 			return this.each(function (i) {
 
 				var $this = $(this),
-					data = $this.data('rejoinder');
+					data = $this.data('modalHome');
 				
 				if (!data) {
-					$this.data('rejoinder', settings);
+					$this.data('modalHome', settings);
 				}
                 
-                updateHeaderTable($this);
-                
-                $(window).on('resize', { elem: $this }, function (e) { updateHeaderTable(e.data.elem); });
-                
 			});
+            */
 		},
-		destroy : function () {
-			
+        hide: function () {
+            console.log('arguments', arguments);
+            $('.jhm-modal').trigger('jhm.modal.close');
+        },
+        destroy : function () {
+			/*
 			return this.each(function () {
 			
 				var $this = $(this),
-					data = $this.data('rejoinder');
+					data = $this.data('modalHome');
 				
 				data.rejoinder.remove();
 				
 			});
+            */
 			
 		}
 	};
-    */
-    // private functions
     
-
+    // private functions
+    var createBg = function () {
+        return $('body').append('<div class="jhm-modal-bg" />');
+    };
+    
+    var createModal = function (content) {
+        return $('.jhm-modal-bg').before('<div class="jhm-modal"><div class="jhm-modal-close">x</div><div class="jhm-modal-content"></div></div>');
+    };
+    
+    var revealModal = function (modal) {
+        console.log('reveal');
+        
+        $('.jhm-modal-bg').show();
+        $('.jhm-modal').fadeIn(500);
+    };
+    
+    var hideModal = function (modal) {
+        console.log('hide');
+        
+        $('.jhm-modal').fadeOut(500);
+        $('.jhm-modal-bg').delay(500).hide();
+    };
+    
+    var populateModal = function (content) {
+        $('.jhm-modal-content').html(content);
+    };
+    
 	$.fn.modalHome = function (method) {
 		var $this = $(this);
         
@@ -51,7 +123,7 @@
 		} else if (typeof method === 'object' || !method) {
 			return methods.init.apply(this, arguments);
 		} else {
-			$.error('Method ' + method + ' does not exist on jQuery.rejoinder');
+			$.error('Method ' + method + ' does not exist on jQuery.modalHome');
 		}
         
 		return this;
--- tests/test-runner.js
+++ tests/test-runner.js
@@ -1,20 +1,67 @@
 /*jslint regexp: true, nomen: true, sloppy: true, debug: true */
-/*global jQuery, require, test, equal, module, ok, start, asyncTest */
-test("Test Runner is loaded", function () {
-    var value = "hello";
-    equal(value, "hello", "We expect value to be hello");
+/*global $, window, jQuery, require, test, equal, notEqual, module, ok, start, stop, setTimeout, asyncTest */
+// tests
+module("Plugin Setup");
+test("ModalHome plugin loaded", function () {
+    ok($.isFunction($.fn.modalHome), "ModalHome plugin is not loaded");
 });
 
-module("Plugin Setup");
-asyncTest("jQuery loaded through Require.js", 1, function () {
-    require(['jquery'], function ($) {
-        equal($.fn.jquery, "1.8.3", "jQuery 1.8.3 is not loaded");
-        start();
-    });
+module("Plugin Generation", {
+    teardown: function () {
+        // cleanup page elements
+        $('.jhm-modal-bg').remove();
+        $('.jhm-modal').remove();
+    }
 });
-asyncTest("ModalHome plugin loaded through Require.js", 1, function () {
-    require(['jquery', 'modalHome'], function ($, mh) {
-        ok($.isFunction($.modalHome), "ModalHome plugin is not loaded");
+asyncTest("ModalHome modal generated", 7, function () {
+    var $fixture = $("#qunit-fixture");
+    $fixture.modalHome({content: 'Lorem Ipsum'});
+    
+    notEqual($('.jhm-modal-bg').length, 0, "Background div present");
+    notEqual($('.jhm-modal-bg:visible').length, 0, "Background div visible");
+    notEqual($('.jhm-modal').length, 0, "Modal panel is present");
+    notEqual($('.jhm-modal:visible').length, 0, "Modal panel is visible");
+    equal($('.jhm-modal').html(), '<div class=\"jhm-modal-close\">x</div><div class=\"jhm-modal-content\">Lorem Ipsum</div>', "Modal content not present");
+    
+    //$().modalHome('hide');
+    $('.jhm-modal-close').trigger('click');
+    
+    setTimeout(function () {
+        equal($('.jhm-modal-bg:visible').length, 0, "Background div visible after close");
+        equal($('.jhm-modal:visible').length, 0, "Modal panel is visible after close");
         start();
-    });
+    }, 2000);
+});
+asyncTest("ModalHome ESC closes modal", 2, function () {
+    var $fixture = $("#qunit-fixture");
+    $fixture.modalHome({content: 'Lorem Ipsum'});
+    
+    setTimeout(function () {
+        var e = jQuery.Event("keyup");
+        e.which = 27; // # Some key code value
+        $('body').trigger(e);
+    }, 1000);
+    
+    setTimeout(function () {
+        equal($('.jhm-modal-bg:visible').length, 0, "Background div should not be visible");
+        equal($('.jhm-modal:visible').length, 0, "Modal panel should not be visible");
+        start();
+    }, 2000);
+});
+asyncTest("ModalHome background click closes modal", 2, function () {
+    var $fixture = $("#qunit-fixture");
+    $fixture.modalHome({content: 'Lorem Ipsum'});
+
+    setTimeout(function () {
+        console.log('trigger click');
+        $('.jhm-modal-bg').trigger('click');
+    }, 1000);
+    
+    setTimeout(function () {
+        console.log('trigger tests');
+        console.log($('.jhm-modal-bg'));
+        equal($('.jhm-modal-bg:visible').length, 0, "Background div should not be visible");
+        equal($('.jhm-modal:visible').length, 0, "Modal panel should not be visible");
+        start();
+    }, 2000);
 });
